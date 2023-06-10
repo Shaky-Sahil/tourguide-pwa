b533037956dece02ad4c1f866bdd7c799dd9d1ee
Sahil Shaikh <sahilpk81@gmail.com>
2023-06-10 11:51:45 +0530

    Update App.js
    dark matter style added

--- src/App.js
+++ src/App.js
@@ -72,8 +72,8 @@ axios.get('')
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    url={`https://api.maptiler.com/maps/${MAP_ID}/256/{z}/{x}/{y}.png?key=${MAPTILER_ACCESS_TOKEN}`}
-
+    //url={`https://api.maptiler.com/maps/${MAP_ID}/256/{z}/{x}/{y}.png?key=${MAPTILER_ACCESS_TOKEN}`}
+    url = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
   />
   <Marker position={locations[0]}>
   </Marker>
